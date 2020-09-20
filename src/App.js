f23fcd88c32c45795b0f666b1adc62d6a4970cfd
Rodrigo <rodrigo.hammes7@gmail.com>
2020-09-20 14:39:04 -0300

    chore: config theme provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 
 import Layout from './components/Layout';
 import Routes from './routes';
 
-const App = () => (
-  <React.Suspense fallback={<div>Carregando...</div>}>
-    <Layout>
-      <Router>
-        <Routes />
-      </Router>
-    </Layout>
-  </React.Suspense>
-);
+const App = () => {
+  const theme = {
+    light: {
+      primary: '#3C1361',
+      lighter: '#52307C'
+    }
+  };
+
+  return (
+    <React.Suspense fallback={<div>Carregando...</div>}>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Router>
+            <Routes />
+          </Router>
+        </Layout>
+      </ThemeProvider>
+    </React.Suspense>
+  );
+};
 
 export default App;
