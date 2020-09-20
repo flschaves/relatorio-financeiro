2f1d2b8bf9b8bdf0f9082bc955aa1def9b475382
Filipe Chaves <flschaves@gmail.com>
2020-09-20 14:58:52 -0300

    feat: add box-sizing: border-box

--- src/styles/index.js
+++ src/styles/index.js
@@ -4,6 +4,10 @@ import { normalize } from "styled-normalize";
 export const GlobalStyle = createGlobalStyle`
 	${normalize}
 
+	*, *::after, *::before {
+		box-sizing: border-box;
+	}
+	
 	body {
 		padding: 0;
 	}
