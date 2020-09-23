bcd07f1938cfbc4ab4142064792f1f46a01a4333
Filipe Chaves <flschaves@gmail.com>
2020-09-23 20:15:14 -0300

    feat: create NotFound page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 const About = React.lazy(() => import("../pages/About"));
+const NotFound = React.lazy(() => import("../pages/NotFound"));
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={About} />
       <Route exact path="/salario-liquido" component={About} />
-      <Route component={About} />
+      <Route component={NotFound} />
     </Switch>
   )
 };
